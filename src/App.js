d8f00def774d754dd8d0d132519301cef429c815
Ted <tugrulerdemdogru@gmail.com>
2022-06-21 22:02:01 +0300

    added useFetch hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import useFetch from './useFetch';
 
 function App() {
+
+  const base_url = "https://catfact.ninja/fact";
+
+  const { data, error, loading } = useFetch(base_url);
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        {loading && <h1>...loading</h1>}
+        {error && <h3>error</h3>}
+        {data.fact}
+      </div>
     </div>
   );
 }
